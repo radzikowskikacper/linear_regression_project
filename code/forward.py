c83c0c34a2562669f4d3b7e886707a423bd82ed5
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-12-28 01:56:24 +0100

    forward prop

--- code/forward.py
+++ code/forward.py
@@ -1,5 +1,14 @@
-def forward():
-    pass
+import numpy as np
+
+def forward(trX, L, W, b, activate_pointer):
+    A = {0: trX.T}
+    Z = dict()
+
+    for l in range(1, L):
+        Z[l] = np.dot(A[l - 1], W[l]) + b[l]
+        A[l] = activate_pointer(Z[l])
+
+    return A, Z
 
 def calculate_cost():
     pass
--- code/nn.py
+++ code/nn.py
@@ -4,6 +4,7 @@ from random import shuffle
 from forward import forward, calculate_cost
 from backward import backprop, update_parameters
 from function_to_estimate import generate_data
+from relu import relu
 
 layers = [2, 5, 4, 1] # number of units in each layer (layers[0] - input layer)
 L = len(layers) # number of layers
@@ -18,25 +19,25 @@ X = np.floor(np.random.rand(2, 1000) * 100) # inputs
 # columns - training example, above we have 1000 training examples
 #Y = function1(X) # outputs
 
-Z = {}
 m = np.shape(X)[1] # number of training examples
 
 # parameters initialization
 for l in range(1,len(layers)):
-    W[l] = np.random.randn(layers[l], layers[l-1]) * 0.01 # or / np.sqrt(layers[l])
-    b[l] = np.zeros((layers[l], 1))
+    W[l] = np.random.randn(layers[l-1], layers[l]) * 0.01 # or / np.sqrt(layers[l])
+    b[l] = np.zeros((1, layers[l]))
 
 data = generate_data(0, 1000, 0.5, 0, 1000, 0.5)
 data_indexes = np.random.choice([0, 1, 2], p = [0.8, 0.1, 0.1], size = (1, data.shape[1]))
 training_data = data[:, (data_indexes == 0)[0]]
 testing_data = data[:, (data_indexes == 1)[0]]
 validation_data = data[:, (data_indexes == 2)[0]]
+trX = training_data[0:2,:]
+trY = training_data[2,:]# or 3
 
 #training
 for i in range(0, number_of_iterations):
     #forward propagation through all the layers
-    for l in range(1, L):
-        A = forward()
+    A, Z = forward(trX, L, W, b, relu)
 
     calculate_cost()
 
