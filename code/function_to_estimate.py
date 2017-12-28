54b1962903503f107bf132e34dd5b9141038812f
Kacper Radzikowski <radzikowskikacper@gmail.com>
2017-12-28 01:05:18 +0100

    generacja danych, podzial na zbiory

--- code/function_to_estimate.py
+++ code/function_to_estimate.py
@@ -4,16 +4,16 @@ import itertools as ittools
 #Example function to be estimated by NN:
 #  y = sin(x1) * cos(x2)
 def function1(X):
-    return np.reshape(np.multiply(np.sin(X[:, 0]), np.cos(X[:, 1])), (-1, 1))
+    return np.reshape(np.multiply(np.sin(X[0, :]), np.cos(X[1, :])), (1, -1))
 
 #Example function to be estimated by NN:
 #  y = x1^2 + x2^2
 def function2(X):
-    return np.reshape(np.multiply(X[:, 0], X[:, 0]) + np.multiply(X[:, 1], X[:, 1]), (-1, 1))
+    return np.reshape(np.multiply(X[0, :], X[0, :]) + np.multiply(X[1, :], X[1, :]), (1, -1))
 
 def generate_data(x1min, x1max, x1step, x2min, x2max, x2step):
     arr = np.array(list(ittools.product(np.arange(x1min, x1max, x1step), np.arange(x2min, x2max, x2step))))
-    arr = np.reshape(arr, (-1, 2))
-    arr = np.hstack((arr, function1(arr), function2(arr)))
+    arr = np.reshape(arr, (2, -1))
+    arr = np.vstack((arr, function1(arr), function2(arr)))
     return arr
 
--- code/nn.py
+++ code/nn.py
@@ -1,6 +1,9 @@
+import numpy as np
+
+from random import shuffle
 from forward import forward, calculate_cost
 from backward import backprop, update_parameters
-from function_to_estimate import *
+from function_to_estimate import generate_data
 
 layers = [2, 5, 4, 1] # number of units in each layer (layers[0] - input layer)
 L = len(layers) # number of layers
@@ -10,11 +13,10 @@ number_of_iterations = 100
 learning_rate = 0.1
 
 np.random.seed(100) # to have the same results (testing) - remove in final solution
-
 X = np.floor(np.random.rand(2, 1000) * 100) # inputs
 # rows - features, in our example one example is two-dimentional vector
 # columns - training example, above we have 1000 training examples
-Y = function1(X) # outputs
+#Y = function1(X) # outputs
 
 Z = {}
 m = np.shape(X)[1] # number of training examples
@@ -23,6 +25,12 @@ m = np.shape(X)[1] # number of training examples
 for l in range(1,len(layers)):
     W[l] = np.random.randn(layers[l], layers[l-1]) * 0.01 # or / np.sqrt(layers[l])
     b[l] = np.zeros((layers[l], 1))
+
+data = generate_data(0, 1000, 0.5, 0, 1000, 0.5)
+data_indexes = np.random.choice([0, 1, 2], p = [0.8, 0.1, 0.1], size = (1, data.shape[1]))
+training_data = data[:, (data_indexes == 0)[0]]
+testing_data = data[:, (data_indexes == 1)[0]]
+validation_data = data[:, (data_indexes == 2)[0]]
 
 #training
 for i in range(0, number_of_iterations):
