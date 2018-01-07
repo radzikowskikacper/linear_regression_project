7494961ebb8d868e5821d90435fdbe9a65bf330a
Michal Przysucha <Michal.Przysucha@adform.com>
2018-01-08 00:07:39 +0100

    sprawozdanie prawie calosc

--- code/evaluate.py
+++ code/evaluate.py
@@ -19,7 +19,13 @@ def print_metrics(layers, W, b, activation_fun):
     tsA, _ = forward(tsX, L, W, b, activation_fun['forward'])
     print("Regression metrics:")
     print("MSE:", np.power(tsA[L] - tsY, 2).mean() / np.shape(tsA[L])[1])
-    print("R2: ", np.sum(np.power(tsA[L] - tsY.mean(), 2)) / np.sum(np.power(tsY - tsY.mean(), 2)))
+    # print("R2: ", np.sum(np.power(tsA[L] - tsY.mean(), 2)) / np.sum(np.power(tsY - tsY.mean(), 2)))
+    SSreg = np.sum(np.power(tsA[L] - tsY.mean(), 2))
+    SSres = np.sum(np.power(tsA[L] - tsY, 2))
+    # SStot = np.sum(np.power(tsY - tsY.mean(), 2))
+    SStot = SSres + SSreg
+    R2 = 1 - SSres / SStot
+    print("R2: ", R2)
     pearson, pearson_pvalue = pearsonr(tsA[L].reshape(tsA[L].shape[1]), tsY.reshape(tsY.shape[1]))
     spearman, spearman_pvalue = spearmanr(tsA[L].reshape(tsA[L].shape[1]), tsY.reshape(tsY.shape[1]))
     print("Pearson: {:.3f}, p-value: {:.3f}".format(pearson, pearson_pvalue))
